c2f21e2cf23375276089d43df762babe8ae8b55f
Daniil <daniildu437@gmail.com>
2025-04-06 17:35:08 +0200

    Added pause menu.

---
+++ Horror3D/Assets/Scripts/UI/PauseGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject quit;
+
+    public bool on;
+    public bool off;
+
+    void Start()
+    {
+        menu.SetActive(false);
+        off = true;
+        on = false;
+    }
+
+    void Update()
+    {
+        if (off && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            menu.SetActive(true);
+            off = false;
+            on = true;
+        }
+
+        else if (on && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 1;
+            menu.SetActive(false);
+            off = true;
+            on = false;
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
