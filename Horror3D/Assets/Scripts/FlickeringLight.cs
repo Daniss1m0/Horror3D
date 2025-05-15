5828d6bfdeee9787a9616b01484d0dd6f08e91b9
Daniil <daniildu437@gmail.com>
2025-05-15 15:02:57 +0200

    Added more lamps with lighting in scene and some decorations

--- Horror3D/Assets/Scripts/FlickeringLight.cs
+++ Horror3D/Assets/Scripts/FlickeringLight.cs
@@ -2,14 +2,27 @@ using UnityEngine;
 
 public class FlickeringLight : MonoBehaviour
 {
+    [Header("Light Settings")]
     public Light flickerLight;
-    public float minDelay = 0.5f;
-    public float maxDelay = 2.0f;
+    public float minDelay = 0.05f;
+    public float maxDelay = 0.3f;
+
+    [Header("Emission Settings")]
+    public Renderer emissionRenderer;
+    public Color emissionOnColor = Color.white;
+    public Color emissionOffColor = Color.black;
+
+    private Material _material;
 
     void Start()
     {
         if (flickerLight == null)
             flickerLight = GetComponent<Light>();
+
+        if (emissionRenderer != null)
+        {
+            _material = emissionRenderer.material;
+        }
 
         StartCoroutine(FlickerRoutine());
     }
@@ -18,9 +31,23 @@ public class FlickeringLight : MonoBehaviour
     {
         while (true)
         {
-            flickerLight.enabled = !flickerLight.enabled;
+            bool isOn = !flickerLight.enabled;
+            flickerLight.enabled = isOn;
+
+            if (_material != null)
+            {
+                SetEmission(isOn);
+            }
+
             float delay = Random.Range(minDelay, maxDelay);
             yield return new WaitForSeconds(delay);
         }
+    }
+
+    void SetEmission(bool isOn)
+    {
+        Color targetColor = isOn ? emissionOnColor : emissionOffColor;
+        _material.SetColor("_EmissionColor", targetColor);
+        DynamicGI.SetEmissive(emissionRenderer, targetColor);
     }
 }
